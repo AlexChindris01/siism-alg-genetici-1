ff5da7958d863e40cbb37440f3082d54fc426641
AlexChindris01 <chindrisalex01@gmail.com>
2022-10-04 19:44:32 +0300

    -

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    # nr gene
+    # populatie
+    # functie fitness
+    # nr epoci
+    # domeniu valori gene
+    # functie selectia indivizilor
+    # functie imperechere
+    # functie generare individ
+    # functie mutatie
+
+    # gen. pop. -> ___ fitness -> nr epoci
