cc4ea6489db300c1a1d49388e9b3429c1342e045
AlexChindris01 <chindrisalex01@gmail.com>
2022-10-04 19:48:47 +0300

    -

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
-    # nr gene
-    # populatie
-    # functie fitness
-    # nr epoci
-    # domeniu valori gene
-    # functie selectia indivizilor
+    # nr gene - 50
+    # populatie - 4000
+    # functie fitness - sum(val. gene)
+    # nr epoci - 500
+    # domeniu valori gene [0, 99]
+    # functie selectia indivizilor - random 3 indivizi - sel. cel mai bun fitness
     # functie imperechere
     # functie generare individ
     # functie mutatie
